9d4f020e3d9a8bd8bcaa4c07332d6446dc875079
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-12-21 12:06:52 +0700

    dinamis halaman pertanyaan

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,7 +1,7 @@
 <script setup>
 import QuizContent from '@/components/QuizContent.vue';
 import QuizHeader from '@/components/QuizHeader.vue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import quizes from '../data/quizes.json';
 import { useRoute } from 'vue-router';
 const router = useRoute(); // fungsi router untuk mengambil id
@@ -13,13 +13,22 @@ const quiz = quizes.find((q) => q.id === quizId);
 // console.log(quiz);
 // console.log(quiz.questions[0]);
 
+// dinamis halaman nomor pertanyaan
 const currentQuestionIndex = ref(0);
+const questionPage = ref(`${currentQuestionIndex.value + 1} / ${quiz.questions.length}`);
+watch(
+    () => currentQuestionIndex.value,
+    () => {
+        questionPage.value = `${currentQuestionIndex.value + 1} / ${quiz.questions.length}`
+    }
+)
 
 </script>
 <template>
     <!-- header -->
-     <QuizHeader/>
+     <QuizHeader :questionPage="questionPage"/>
     <!-- quiz content -->
      <QuizContent :question="quiz.questions[currentQuestionIndex]"/>
+     <button @click="currentQuestionIndex++" :disabled="currentQuestionIndex === quiz.questions.length - 1">Next</button>
 </template>
 
